855a07f143f5680ebdd1c5dd916900893cae56ed
Ataa <ataa@gmail.com>
2023-07-02 15:27:41 +0530

    added navigation or layout

---
+++ src/components/AppTitle.jsx
@@ -0,0 +1,13 @@
+export default function AppTitle(props) {
+  const {
+    title = 'Box Office',
+    subtitle = 'Are You looking for an movie or an actor ',
+  } = props;
+
+  return (
+    <div>
+      <h1>{title}</h1>
+      <p>{subtitle}</p>
+    </div>
+  );
+}
